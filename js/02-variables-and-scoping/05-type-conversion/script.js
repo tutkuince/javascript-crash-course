2d710ed9e784acd91c1a4bfd9e94ec9ad7e4f275
Tutku Ince <tutku.ince@outlook.com>
2023-09-15 00:40:06 +0300

    Type Conversion

---
+++ js/02-variables-and-scoping/05-type-conversion/script.js
@@ -0,0 +1,24 @@
+// Convert String to Number
+let amount = "100";
+console.log(amount, typeof amount);
+
+// amount = parseInt(amount);
+// amount =+ amount;
+amount = Number(amount);
+// console.log(amount, typeof amount);
+
+
+// Convert Number to String
+// amount = amount.toString();
+amount = String(amount);
+
+// Convert String to Decimal
+amount = "99.5";
+amount = parseFloat(amount);
+
+// Convert Number to Boolean
+amount = 1;
+amount = Boolean(amount);
+
+console.log(amount, typeof amount);
+
