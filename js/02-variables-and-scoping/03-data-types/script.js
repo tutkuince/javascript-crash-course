2529b33c0d46575dd3c0c4550848a176e697a8f6
Tutku Ince <tutku.ince@outlook.com>
2023-09-14 23:40:42 +0300

    Data Types Definition:

---
+++ js/02-variables-and-scoping/03-data-types/script.js
@@ -0,0 +1,17 @@
+//
+// Primitive Data Types
+//
+// String - Sequence of characters. Must be in quotes or backticks
+// Number - Integers as well as floating-point numbers
+// Boolean - Logical entity / true or false
+// Null - Intentional absence of any object value
+// Undefined - A Variable that has not yet been defined / assigned
+// Symbol - Built-in object whose constructor returns a unique symbol
+// BigInt - Numbers that are greater than the "Number" type can handle
+
+//
+// Reference Types (Objects)
+//
+// Reference types or "objects" are a non-primitive value and when assigned to a variable,
+// the variable is given a reference to that value.
+// Object literals, arrays and functions are all reference types
