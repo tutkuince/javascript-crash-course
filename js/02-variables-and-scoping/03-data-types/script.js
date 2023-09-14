afa132a0a4013957a0961cecf773045a1fe074c8
Tutku Ince <tutku.ince@outlook.com>
2023-09-14 23:43:53 +0300

    Static Typing vs Dynamic Types definition

--- js/02-variables-and-scoping/03-data-types/script.js
+++ js/02-variables-and-scoping/03-data-types/script.js
@@ -15,3 +15,12 @@
 // Reference types or "objects" are a non-primitive value and when assigned to a variable,
 // the variable is given a reference to that value.
 // Object literals, arrays and functions are all reference types
+
+//
+// Static Typing vs Dynamic Types
+//
+// JavaScript is a dynamically-typed language. This means, we do not explicitly define the types for our variables.
+// Many other languages are statically-typed such as C, C++ and Java
+//
+// TypeScript is a superset of JavaScript, which allows static-typing. This can make your code more verbose and less prone to errors.
+// TypeScript may be something you want to look into later on.
