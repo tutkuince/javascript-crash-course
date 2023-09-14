cdee28a5136062c155556d81bae8037bdbb3b880
Tutku Ince <tutku.ince@outlook.com>
2023-09-15 00:02:48 +0300

    Primitive vs Reference Types

--- js/02-variables-and-scoping/04-primitive-vs-reference-types/script.js
+++ js/02-variables-and-scoping/04-primitive-vs-reference-types/script.js
@@ -6,3 +6,23 @@
 //
 // Reference Types: Stored in the heap and accessed by reference
 // Arrays | Functions | Objects
+
+
+// Values are stored on the stack
+const name = "John";
+const age = 30;
+
+// Reference values are stored on the heap
+const person = {
+    name: "Tutku",
+    age: 34
+};
+
+let newName = name;
+newName = "Johnathan";
+
+let newPerson = person;
+newPerson.name = "Bradley";
+
+console.log(name, newName);
+console.log(person, newPerson);
